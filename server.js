20a97c368373502b10943f212413e25f0ad939f3
Harizo ANDRIANAIVO <124563616+Eclisher@users.noreply.github.com>
2023-08-07 20:39:27 +0300

    fix: Update server.js

--- server.js
+++ server.js
@@ -538,6 +538,345 @@ app.post('/provinces', (_req, res) => {
     });
 });
 
+
+//  SELECT MEDIUM 
+
+app.get('/reservations-count', async (_req, res) => {
+  try {
+    const query = `
+      SELECT c.id_client, c.first_name, c.last_name, COUNT(r.id_reservation) AS total_reservations
+      FROM client c
+      LEFT JOIN reservation r ON c.id_client = r.id_client
+      GROUP BY c.id_client, c.first_name, c.last_name;
+    `;
+
+    const result = await client.query(query);
+    const reservationsCount = result.rows;
+
+    res.json(reservationsCount);
+  } catch (err) {
+    console.error('Erreur lors de l\'exécution de la requête :', err);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+});
+
+// -- Afficher la liste des chambres qui seront libres demain :
+app.get('/availableroom', async (_req, res) => {
+  try {
+    const query = `
+    SELECT r.number, r.room_type
+    FROM room r
+    WHERE r.id_room NOT IN (
+        SELECT res.id_room
+        FROM reservation res
+        WHERE res.date_arrived <= CURRENT_DATE + INTERVAL '1 day'
+        AND res.leaving_date >= CURRENT_DATE + INTERVAL '1 day'
+    );
+    `;
+
+    const result = await client.query(query);
+    const availableroom = result.rows;
+
+    res.json(availableroom);
+  } catch (err) {
+    console.error('Erreur lors de l\'exécution de la requête :', err);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+});
+
+
+// -- Afficher le nombre total de réservations par type de chambre (par exemple : VIP : 30 réservations, Simple : 40 réservations...) :
+app.get('/total_reservation_wtih_type', async (_req, res) => {
+  try {
+    const query = `
+    SELECT r.number, r.room_type
+    FROM room r
+    WHERE r.id_room NOT IN (
+        SELECT res.id_room
+        FROM reservation res
+        WHERE res.date_arrived <= CURRENT_DATE + INTERVAL '1 day'
+        AND res.leaving_date >= CURRENT_DATE + INTERVAL '1 day'
+    );
+    `;
+
+    const result = await client.query(query);
+    const total_reservation_wtih_type = result.rows;
+
+    res.json(total_reservation_wtih_type);
+  } catch (err) {
+    console.error('Erreur lors de l\'exécution de la requête :', err);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+});
+
+// -- Afficher la liste des chambres qui répondent à de multiples critères
+
+app.get('/listroom', async (_req, res) => {
+  try {
+    const query = `
+    SELECT r.number, r.room_type
+    FROM room r
+    JOIN room_features rf ON r.id_room_features = rf.id_rooms_features
+    WHERE rf.wifi_available = true
+    AND rf.hot_water = true
+    AND rf.mini_bar=true
+    AND rf.flat_screen=true
+    AND rf.room_service=true
+    AND r.capacity_room >= 5;
+    `;
+
+    const result = await client.query(query);
+    const listroom = result.rows;
+
+    res.json(listroom);
+  } catch (err) {
+    console.error('Erreur lors de l\'exécution de la requête :', err);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+});
+
+// -- Afficher le nombre total des réservations par hôtel :
+app.get('/total_reservation', async (_req, res) => {
+  try {
+    const query = `
+    SELECT r.number, r.room_type
+    FROM room r
+    JOIN room_features rf ON r.id_room_features = rf.id_rooms_features
+    WHERE rf.wifi_available = true
+    AND rf.hot_water = true
+    AND rf.mini_bar=true
+    AND rf.flat_screen=true
+    AND rf.room_service=true
+    AND r.capacity_room >= 5;
+    `;
+
+    const result = await client.query(query);
+    const total_reservation = result.rows;
+
+    res.json(total_reservation);
+  } catch (err) {
+    console.error('Erreur lors de l\'exécution de la requête :', err);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+});
+
+// -- Afficher chaque client, et le nombre de fois où il a annulé une réservation :
+
+app.get('/cancel_reservation', async (_req, res) => {
+  try {
+    const query = `
+    SELECT c.id_client, c.first_name, c.last_name, COUNT(*) AS cancellations
+    FROM client c
+    JOIN reservation r ON c.id_client = r.id_client
+    WHERE r.is_cancel = true
+    GROUP BY c.id_client, c.first_name, c.last_name`;
+
+    const result = await client.query(query);
+    const cancel_reservation = result.rows;
+
+    res.json(cancel_reservation);
+  } catch (err) {
+    console.error('Erreur lors de l\'exécution de la requête :', err);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+});
+
+// -- Afficher les hôtels, avec le nombre de chambres par hôtel :
+
+app.get('/rooms_hotel', async (_req, res) => {
+  try {
+    const query = `
+    SELECT h.hotel_name, COUNT(r.id_room) AS total_rooms
+    FROM hotel h
+    LEFT JOIN room r ON h.id_hotel = r.id_hotel
+    GROUP BY h.hotel_name;`;
+
+    const result = await client.query(query);
+    const rooms_hotel = result.rows;
+
+    res.json(rooms_hotel);
+  } catch (err) {
+    console.error('Erreur lors de l\'exécution de la requête :', err);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+});
+
+
+// --Afficher la liste des chambres occupées actuellement
+
+app.get('/rooms_disenable', async (_req, res) => {
+  try {
+    const query = `
+    SELECT *
+    FROM room
+    WHERE id_room IN (
+        SELECT id_room
+        FROM reservation
+        WHERE leaving_date >= CURRENT_DATE
+    );
+    `;
+
+    const result = await client.query(query);
+    const rooms_disenable = result.rows;
+
+    res.json(rooms_disenable);
+  } catch (err) {
+    console.error('Erreur lors de l\'exécution de la requête :', err);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+});
+
+// --Afficher les chambres les moins réservés d'un hotel donné (2 par exemple)
+
+app.get('/room_moins_reserve', async (_req, res) => {
+  try {
+    const query = `
+    SELECT r.id_room, r.number, r.room_type, COUNT(re.id_reservation) AS total_reservations
+    FROM room r
+    LEFT JOIN reservation re ON r.id_room = re.id_room
+    WHERE r.id_hotel = 2
+    GROUP BY r.id_room, r.number, r.room_type
+    ORDER BY total_reservations ASC;
+    `;
+
+    const result = await client.query(query);
+    const room_moins_reserve = result.rows;
+
+    res.json(room_moins_reserve);
+  } catch (err) {
+    console.error('Erreur lors de l\'exécution de la requête :', err);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+});
+
+// --Afficher les chambres les plus réservés d'un hotel donné (2 par exemple)
+app.get('/room_plus_reserve', async (_req, res) => {
+  try {
+    const query = `
+    SELECT r.number,r.room_type, COUNT(*) AS total_reservations
+    FROM room r
+    JOIN reservation re ON r.id_room = re.id_room
+    WHERE r.id_hotel = 2
+    GROUP BY r.number, r.room_type
+    ORDER BY total_reservations DESC;
+    `;
+
+    const result = await client.query(query);
+    const room_plus_reserve = result.rows;
+
+    res.json(room_plus_reserve);
+  } catch (err) {
+    console.error('Erreur lors de l\'exécution de la requête :', err);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+});
+
+// --Afficher la liste des promotions en cours actuellement
+app.get('/promotion_en_cours', async (_req, res) => {
+  try {
+    const query = `
+    SELECT p.name, p.begin_date, p.end_date, p.percent
+    FROM promotion p
+    WHERE CURRENT_DATE BETWEEN p.begin_date AND p.end_date;
+    `;
+
+    const result = await client.query(query);
+    const promotion_en_cours = result.rows;
+
+    res.json(promotion_en_cours);
+  } catch (err) {
+    console.error('Erreur lors de l\'exécution de la requête :', err);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+});
+
+// Afficher la liste des clients, avec le nombre de reservations, le nombre de reservations annulées, et le total d'argent que le client a payé
+
+app.get('/client_reservation', async (_req, res) => {
+  try {
+    const query = `
+    SELECT
+    client.id_client,
+    client.first_name,
+    client.last_name,
+    COUNT(DISTINCT reservation.id_reservation) AS total_reservations,
+    COUNT(CASE WHEN reservation.is_cancel = true THEN 1 ELSE NULL END) AS total_cancelled_reservations,
+    COALESCE(SUM(payment.amount_paid), 0) AS total_amount_paid
+FROM
+    client
+LEFT JOIN
+    reservation ON client.id_client = reservation.id_client
+LEFT JOIN
+    payment ON client.id_client = payment.id_client
+GROUP BY
+    client.id_client,
+    client.first_name,
+    client.last_name;
+    `;
+
+    const result = await client.query(query);
+    const client_reservation = result.rows;
+
+    res.json(client_reservation);
+  } catch (err) {
+    console.error('Erreur lors de l\'exécution de la requête :', err);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+});
+
+
+// -Afficher la liste des payements avec le nom et le prénom du réceptionniste que les a reçus
+app.get('/payement', async (_req, res) => {
+  try {
+    const query = `
+    SELECT p.name, p.begin_date, p.end_date, p.percent
+    FROM promotion p
+    WHERE CURRENT_DATE BETWEEN p.begin_date AND p.end_date;
+    `;
+
+    const result = await client.query(query);
+    const payement = result.rows;
+
+    res.json(payement);
+  } catch (err) {
+    console.error('Erreur lors de l\'exécution de la requête :', err);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+});
+
+
+// Afficher la liste des chambres, avec leur prix brut, et leur prix net (c'est-à-dire leur prix en appliquant toutes les réductions qui sont appliquées aujourd'hui)
+
+app.get('/room_price', async (_req, res) => {
+  try {
+    const query = `
+    SELECT
+    r.id_room,
+    r.number,
+    r.room_type,
+    r.capacity_room,
+    p.cost_per_night AS prix_brut,
+    p.cost_per_night * (1 - (pr.percent / 100)) AS prix_net
+FROM
+    room r
+    LEFT JOIN price p ON r.id_room = p.id_room
+    LEFT JOIN have h ON r.id_room = h.id_room
+    LEFT JOIN promotion pr ON h.id_promotion = pr.id_promotion
+WHERE
+    CURRENT_DATE BETWEEN pr.begin_date AND pr.end_date;
+    `;
+
+    const result = await client.query(query);
+    const room_price = result.rows;
+
+    res.json(room_price);
+  } catch (err) {
+    console.error('Erreur lors de l\'exécution de la requête :', err);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+});
+
+
 // Démarrez le serveur
 app.listen(port, () => {
   console.log(`Le serveur est en cours d'exécution sur http://localhost:${port}`);
