8209606389d114f333c6359a3ed0d072a56c4e8a
Miora <MioraNomenjanahary>
2023-08-07 14:42:27 +0300

    fix: update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -3,7 +3,7 @@ function checkCredentials() {
         const email = document.getElementById('email-field').value;
         const password = document.getElementById('password-field').value;
         if (email.includes('admin') && password === 'admin') {
-            window.location.assign ('./dashboard/template/index.html')
+            window.location.assign ('./dashboard/index.html')
         } else if ( email.includes("client")){
             window.location.assign('./index.html')
         }   
